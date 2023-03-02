1082d6b2d4e749bb88ff80398d198e408f41319c
Mauricio <mau04@outlook.es>
2023-03-01 22:59:14 -0600

    first commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CrudNet7MVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudNet7MVC.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        //Agregar los modelos aqui
+        public DbSet<Contacto> Contacto { get; set; }
+    }
+}
---
+++ Migrations/20230301035538_MigracionInicial2Contacto.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CrudNet7MVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigracionInicial2Contacto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Contactos",
+                table: "Contactos");
+
+            migrationBuilder.RenameTable(
+                name: "Contactos",
+                newName: "Contacto");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Contacto",
+                table: "Contacto",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Contacto",
+                table: "Contacto");
+
+            migrationBuilder.RenameTable(
+                name: "Contacto",
+                newName: "Contactos");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Contactos",
+                table: "Contactos",
+                column: "Id");
+        }
+    }
+}
